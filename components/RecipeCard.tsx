a896ac1556127b4dcfa8068add8a972a881fd642
Joseph Stott <josephdanielstott@gmail.com>
2026-06-04 23:14:02 -0700

    Improve recipe card content layout

--- components/RecipeCard.tsx
+++ components/RecipeCard.tsx
@@ -30,20 +30,23 @@ export default function RecipeCard(props: RecipeCardProps) {
             >
                 {props.timeCategory.charAt(0).toUpperCase() + props.timeCategory.slice(1)}
             </span>
-            <div className="relative flex items-center justify-center">
-                <span>{props.title}</span>
+            <div className="flex flex-col gap-2">
+                <div className="relative flex items-center justify-center">
+                    <span>{props.title}</span>
 
-                {props.isFavorite && (
-                    <span className="absolute right-4 text-xl leading-none text-zinc-100">
-                    ★
+                    {props.isFavorite && (
+                        <span className="absolute right-4 text-xl leading-none text-zinc-100">
+                            ★
+                        </span>
+                    )}
+                </div>
+
+                {props.matchCount !== undefined && props.matchCount > 0 && (
+                    <span className="text-sm text-zinc-400">
+                        {props.matchCount} grocery {props.matchCount === 1 ? "match" : "matches"}
                     </span>
                 )}
             </div>
-            {props.matchCount !== undefined && props.matchCount > 0 && (
-                <span>
-                    {props.matchCount} grocery {props.matchCount === 1 ? "match" : "matches"}
-                </span>
-            )}
         </Link>
     )
 }
