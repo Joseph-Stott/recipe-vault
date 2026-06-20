fa575847f49fdfc553e132759272099d85870e06
Joseph Stott <josephdanielstott@gmail.com>
2026-06-19 23:45:30 -0700

    Improve search bar placeholder text

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -7,7 +7,7 @@ export default function SearchBar(props: SearchBarProps) {
     return (
         <input className="w-full max-w-sm p-2 bg-zinc-900 border border-zinc-400 rounded-lg placeholder:text-center"
           type="search"
-          placeholder="Search recipes"
+          placeholder="Search recipes, ingredients, or categories"
           value={props.searchText}
           onChange={(event) => props.setSearchText(event.target.value)}
         />
