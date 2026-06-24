1b64a0870019cb88b605239d0c101c918706c133
Joseph Stott <josephdanielstott@gmail.com>
2026-06-23 23:23:47 -0700

    Add tooltip to recipe form submit buttom

--- components/RecipeForm.tsx
+++ components/RecipeForm.tsx
@@ -210,6 +210,7 @@ export default function RecipeForm({
                 </ul>
             )}
             <button
+                title={submitButtonText}
                 className="cursor-pointer rounded-lg border border-zinc-600 px-3 py-2 text-sm font-medium hover:bg-zinc-800"
                 onClick={onSubmit}
             >
