810b38218cb37821d91d386123837f53d33b2873
Joseph Stott <josephdanielstott@gmail.com>
2026-06-23 23:27:55 -0700

    Add tooltip to back navigation

--- components/BackButton.tsx
+++ components/BackButton.tsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 export default function BackButton() {
     return (
         <Link 
+            title="Go back"
             href="/"
             className="cursor-pointer rounded-lg border border-zinc-600 px-3 py-2 text-sm font-medium hover:bg-zinc-800"
         >
