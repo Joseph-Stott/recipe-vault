b43b94d9c5aefb01607c4387c7ab59c8b2a35d3c
Joseph Stott <josephdanielstott@gmail.com>
2026-06-24 23:41:13 -0700

    Add tooltip to recipe search input

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -5,11 +5,13 @@ type SearchBarProps = {
 
 export default function SearchBar(props: SearchBarProps) {
     return (
-        <input className="w-full max-w-sm p-2 bg-zinc-900 border border-zinc-400 rounded-lg placeholder:text-center"
-          type="search"
-          placeholder="Search recipes, ingredients, or categories"
-          value={props.searchText}
-          onChange={(event) => props.setSearchText(event.target.value)}
+        <input
+            title="Search recipes by title, ingredient, or time category"
+            className="w-full max-w-sm p-2 bg-zinc-900 border border-zinc-400 rounded-lg placeholder:text-center"
+            type="search"
+            placeholder="Search recipes, ingredients, or categories"
+            value={props.searchText}
+            onChange={(event) => props.setSearchText(event.target.value)}
         />
     )
 }
