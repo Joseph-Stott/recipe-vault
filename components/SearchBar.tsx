8c1e679a6f099489bcd94c05e930b6e4902a74fd
Joseph Stott <josephdanielstott@gmail.com>
2026-06-30 22:49:25 -0700

    AutoFocus recipe search on homepage

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -6,6 +6,7 @@ type SearchBarProps = {
 export default function SearchBar(props: SearchBarProps) {
     return (
         <input
+            autoFocus
             title="Search recipes by title, ingredient, or time category"
             className="w-full max-w-sm p-2 bg-zinc-900 border border-zinc-400 rounded-lg placeholder:text-center"
             type="search"
