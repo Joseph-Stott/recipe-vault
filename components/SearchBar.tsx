f82097040f5e3c35d01037cccca50aa06bdd5156
Joseph Stott <josephdanielstott@gmail.com>
2026-07-06 21:10:02 -0700

    Disable spellcheck and autocomplete for recipe search

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -8,8 +8,10 @@ export default function SearchBar(props: SearchBarProps) {
         <input
             autoFocus
             title="Search recipes by title, ingredient, or time category"
-            className="w-full max-w-sm p-2 bg-zinc-900 border border-zinc-400 rounded-lg placeholder:text-center"
             type="search"
+            spellCheck={false}
+            autoComplete="off"
+            className="w-full max-w-sm p-2 bg-zinc-900 border border-zinc-400 rounded-lg placeholder:text-center"
             placeholder="Search recipes, ingredients, or categories"
             value={props.searchText}
             onChange={(event) => props.setSearchText(event.target.value)}
